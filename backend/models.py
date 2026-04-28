8addd48bc9618dba2247c56fac018607f4c2d6fa
Arkkiyel <dhimasaryahgit@gmail.com>
2026-04-28 18:28:21 +0700

    removed unnecessary comments

--- backend/models.py
+++ backend/models.py
@@ -8,28 +8,27 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
     hashed_password = Column(String)
-    role = Column(String) # 'admin' atau 'kelurahan'
-    kelurahan_name = Column(String, nullable=True) # Spesifik untuk akun kelurahan
+    role = Column(String) # 'admin' or 'kelurahan'
+    kelurahan_name = Column(String, nullable=True) # for 'kelurahan' role
 
 class Complaint(Base):
     __tablename__ = "complaints"
     id = Column(Integer, primary_key=True, index=True)
-    # Lokasi
     kelurahan = Column(String, index=True)
     rt = Column(Integer)
     rw = Column(Integer)
     jalan = Column(String)
     description_location = Column(Text)
     
-    # Isi Keluhan
+    # Complaint description
     complaint_text = Column(Text)
-    photo_url = Column(String) # Bukti dari user
+    photo_url = Column(String) # proof from user
     
     # Status & Admin
     status = Column(String, default="pending") # pending, solved
-    admin_photo_url = Column(String, nullable=True) # Bukti dari admin
-    priority_score = Column(Integer, default=0) # Untuk AI NLP nantinya
-    is_approved = Column(Boolean, default=False) # Approval dari koordinator
+    admin_photo_url = Column(String, nullable=True) # Proof from admin
+    priority_score = Column(Integer, default=0) # From our AI NLP 
+    is_approved = Column(Boolean, default=False) # Approval from kelurahan user
     
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     user_id = Column(Integer, ForeignKey("users.id"))
@@ -37,6 +36,6 @@ class Complaint(Base):
 class Schedule(Base):
     __tablename__ = "schedules"
     id = Column(Integer, primary_key=True, index=True)
-    day = Column(String) # Senin - Minggu
-    time = Column(String) # Format jam
+    day = Column(String) # Value: Senin - Minggu
+    time = Column(String) # Hours format
     kelurahan_target = Column(String)
